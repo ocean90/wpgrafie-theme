bda9b349d043933bee3297593eb1bb4d076f0767
Dominik <dominik.schilling@gmail.com>
2012-06-10 17:53:20 +0200

    Save some li spaces

--- classes/class-ds-wpgrafie-theme-minify.php
+++ classes/class-ds-wpgrafie-theme-minify.php
@@ -5,7 +5,14 @@ class DS_wpGrafie_Theme_Minify extends DS_wpGrafie_Theme {
 	 *
 	 * @var array
 	 */
-	private static $_placeholders = array();
+	private static $_pre_placeholders = array();
+
+	/**
+	 * Placeholder to store <ul></ul> code.
+	 *
+	 * @var array
+	 */
+	private static $_ul_placeholders = array();
 
 	/**
 	 * (Re)store <pre></pre> code and replace it with a placeholder.
@@ -16,24 +23,44 @@ class DS_wpGrafie_Theme_Minify extends DS_wpGrafie_Theme {
 	 * @param  boolean $restore True if code should be restored, false if not. Default: false
 	 * @return string HTML
 	 */
-	private static function save_pre( $data, $restore = false ) {
-		if ( ! $restore ) {
-			return preg_replace_callback(
-				'/<pre\b[^>]*?>(.*?)<\/pre>/is',
-				array( __CLASS__, 'pre_cb' ),
-				$data
-			);
-		} else {
-			return str_replace(
-				array_keys( self::$_placeholders ),
-				array_values( self::$_placeholders ),
-				$data
-			);
+	private static function save_space( $data, $type, $restore = false ) {
+		switch( $type ) {
+			case 'pre' :
+				if ( ! $restore ) {
+					return preg_replace_callback(
+						'/<pre\b[^>]*?>(.*?)<\/pre>/is',
+						array( __CLASS__, 'pre_cb' ),
+						$data
+					);
+				} else {
+					return str_replace(
+						array_keys( self::$_pre_placeholders ),
+						array_values( self::$_pre_placeholders ),
+						$data
+					);
+				}
+				break;
+			case 'ul' :
+				if ( ! $restore ) {
+					return preg_replace_callback(
+						'/<ul\b[^>]*?class="bubbles"[^>]*?>(.*?)<\/ul>/is',
+						array( __CLASS__, 'ul_cb' ),
+						$data
+					);
+				} else {
+					return str_replace(
+						array_keys( self::$_ul_placeholders ),
+						array_values( self::$_ul_placeholders ),
+						$data
+					);
+				}
+				break;
+			default:
 		}
 	}
 
 	/**
-	 * Callback function for save_pre().
+	 * Callback function for save_space().
 	 *
 	 * @since 0.1
 	 *
@@ -41,8 +68,22 @@ class DS_wpGrafie_Theme_Minify extends DS_wpGrafie_Theme {
 	 * @return string Placeholder key
 	 */
 	private static function pre_cb( $match ) {
-		$placeholder = '%WPGRAFIESAVEPRE' . count( self::$_placeholders ) . '%';
-		self::$_placeholders[$placeholder] = $match[0];
+		$placeholder = '%WPGRAFIESAVEPRE' . count( self::$_pre_placeholders ) . '%';
+		self::$_pre_placeholders[$placeholder] = $match[0];
+		return $placeholder;
+	}
+
+	/**
+	 * Callback function for save_space().
+	 *
+	 * @since 0.1
+	 *
+	 * @param  string $match <ul></ul> Code
+	 * @return string Placeholder key
+	 */
+	private static function ul_cb( $match ) {
+		$placeholder = '%WPGRAFIESAVEUL' . count( self::$_ul_placeholders ) . '%';
+		self::$_ul_placeholders[$placeholder] = $match[0];
 		return $placeholder;
 	}
 
@@ -95,9 +136,11 @@ class DS_wpGrafie_Theme_Minify extends DS_wpGrafie_Theme {
 		if ( self::$dev )
 			return $data;
 
-		$data = self::save_pre( $data );
+		$data = self::save_space( $data, 'pre' );
+		$data = self::save_space( $data, 'ul' );
 		$data = self::compressor( $data );
-		$data = self::save_pre( $data, true );
+		$data = self::save_space( $data, 'pre', true );
+		$data = self::save_space( $data, 'ul', true );
 
 		return (string) $data;
 	}
