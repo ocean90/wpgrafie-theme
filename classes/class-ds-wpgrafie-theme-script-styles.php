fd30d97ab316380562c603171b2735ed35b94683
Dominik <dominik.schilling@gmail.com>
2012-07-25 13:43:42 +0200

    bumb

--- classes/class-ds-wpgrafie-theme-script-styles.php
+++ classes/class-ds-wpgrafie-theme-script-styles.php
@@ -16,7 +16,7 @@ class DS_wpGrafie_Theme_Script_Styles extends DS_wpGrafie_Theme {
 	 *
 	 * @var string
 	 */
-	private static $style_version = '0.4.2';
+	private static $style_version = '0.4.3';
 
 	/**
 	 * Initialize scripts.
