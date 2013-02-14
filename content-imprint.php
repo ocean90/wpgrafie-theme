ed28655ac3865c24e58b0dc3ec2d248a79edb266
Dominik Schilling <dominikschilling+git@gmail.com>
2013-02-14 17:19:01 +0100

    Removed the asteroid from footer

--- content-imprint.php
+++ content-imprint.php
@@ -1,2 +1,1 @@
 <p id="imprint"><a href="/imprint/">Impressum</a> / <a href="/privacy/">Datenschutz</a> / <a href="http://wordpress.org">Angetrieben von WordPress</a> / <a href="https://uberspace.de/">Gehostet auf einem Asteroiden</a></p>
-<?php
